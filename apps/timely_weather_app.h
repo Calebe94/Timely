18d62a9bd5eca60905a378901b4d1b230d4da87e
calebe94 <calebe.sep@hotmail.com>
2021-09-13 09:10:16 -0300

    Added stock weather app template

---
+++ apps/timely_weather_app.h
@@ -0,0 +1,40 @@
+/**
+ * @file timely_main.h
+ *
+ */
+
+#ifndef _TIMELY_WEATHER_APP_H_
+#define _TIMELY_WEATHER_APP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../timely.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+void timely_weather_init(void *context);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
