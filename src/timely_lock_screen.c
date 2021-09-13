3fcc0c8711cb3551b605b0cc91784782772ec446
calebe94 <calebe.sep@hotmail.com>
2021-09-12 22:48:59 -0300

    Fixed main screen not updating

--- src/timely_lock_screen.c
+++ src/timely_lock_screen.c
@@ -60,6 +60,7 @@ static void lock_screen_event_handler(lv_event_t * e)
     if(code == LV_EVENT_CLICKED) {
         LV_LOG_USER("Clicked");
         timely_launcher_init();
+        timely_main_update();
     }
     else if(code == LV_EVENT_VALUE_CHANGED) {
         LV_LOG_USER("Toggled");
